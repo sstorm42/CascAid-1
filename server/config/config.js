6e399f7f8261e5df2cd35adbecb6471277b07df7
Riasat Ali <riasat.ali@toptal.com>
2021-02-02 11:45:34 +0600

    type corrected for testing.

--- server/config/config.js
+++ server/config/config.js
@@ -11,13 +11,13 @@ const config = {
         SECRET: 'j54O6D6i46T7or45dAk6S445un4eAklA435702',
         DATABASE: 'mongodb://localhost:27017/cascaid',
         EMAIL_USER: 'test@domain.com',
-        EMAIL_PASSWORD: '12345678',
+        EMAIL_PASSWORD: '12345678ABC',
     },
     test: {
         SECRET: 'j54O6D6i46T7or45dAk6S445un4eAklA435702',
         DATABASE: 'mongodb://localhost:27017/cascaid_test',
         EMAIL_USER: 'test@domain.com',
-        EMAIL_PASSWORD: '12345678',
+        EMAIL_PASSWORD: '12345678ABC',
     },
 };
 
