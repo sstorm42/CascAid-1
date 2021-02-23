19d7a9c0bb6f0711e6f00ca0dc842d935fba24d6
Riasat Ali <riasat.ali@toptal.com>
2021-02-23 22:10:50 +0600

    full connection string

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ const config = {
     },
     default: {
         SECRET: 'j54O6D6i46T7or45dAk6S445un4eAklA435702',
-        DATABASE: 'mongodb://cascaidAdmin:AstonMartinV12DB@192.52.112.70:27017/cascaid',
+        DATABASE: 'mongodb://cascaidAdmin:AstonMartinV12DB@192.53.112.70:27017/cascaid?authSource=cascaid&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
         EMAIL_USER: 'test@domain.com',
         EMAIL_PASSWORD: '12345678ABC',
     },
