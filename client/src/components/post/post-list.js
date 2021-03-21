90e0b3d33d319930b21d76edec30e99ec44cd9ce
Riasat Ali <riasat.ali@toptal.com>
2021-03-21 20:59:58 +0600

    post create bug fix

--- client/src/components/post/post-list.js
+++ client/src/components/post/post-list.js
@@ -28,56 +28,55 @@ const PostList = (props) => {
     const DropdownItemRender = (variant, type, label) => {
         return <Dropdown.Item href={RoutePaths.postCreatePage(type)}>{label}</Dropdown.Item>;
     };
-    if (allPosts && allPosts.length > 0) {
-        return (
-            <Container>
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Select Post Type</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Row>
-                            <Col sm="6">{ButtonRender('info', 'event', 'Event')}</Col>
-                            <Col sm="6">{ButtonRender('info', 'project', 'Project')}</Col>
-                            <Col sm="6">{ButtonRender('info', 'general', 'General Post')}</Col>
-                            <Col sm="6">{ButtonRender('info', 'volunteering', 'Volunteering')}</Col>
-                            <Col sm="6">{ButtonRender('info', 'in-kind', 'In-Kind')}</Col>
-                            <Col sm="6">{ButtonRender('info', 'advocacy', 'Advocacy')}</Col>
-                        </Row>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose} size="sm">
-                            Cancel
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-                <Row>
-                    <Col className="parent-page">
-                        <Row>
-                            <Col sm="6">
-                                <h4>{allPosts.length} Posts Found</h4>
-                            </Col>
-                            <Col sm="6" className="right-align">
-                                <Dropdown>
-                                    <Dropdown.Toggle variant="primary" id="dropdown-basic" size="sm" drop="left">
-                                        <FaPlus /> POST
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        {DropdownItemRender('info', 'event', 'Event')}
-                                        {DropdownItemRender('info', 'project', 'Project')}
-                                        {DropdownItemRender('info', 'general', 'General Post')}
-                                        {DropdownItemRender('info', 'volunteering', 'Volunteering')}
-                                        {DropdownItemRender('info', 'in-kind', 'In-Kind')}
-                                        {DropdownItemRender('info', 'advocacy', 'Advocacy')}
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                                <br />
-                                <Button variant="primary" onClick={handleShow} size="sm">
-                                    Create Post
-                                </Button>
-                            </Col>
-                        </Row>
-                        <br />
+
+    return (
+        <Container>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Select Post Type</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        <Col sm="6">{ButtonRender('info', 'event', 'Event')}</Col>
+                        <Col sm="6">{ButtonRender('info', 'project', 'Project')}</Col>
+                        <Col sm="6">{ButtonRender('info', 'general', 'General Post')}</Col>
+                        <Col sm="6">{ButtonRender('info', 'volunteering', 'Volunteering')}</Col>
+                        <Col sm="6">{ButtonRender('info', 'in-kind', 'In-Kind')}</Col>
+                        <Col sm="6">{ButtonRender('info', 'advocacy', 'Advocacy')}</Col>
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose} size="sm">
+                        Cancel
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <Row>
+                <Col className="parent-page">
+                    <Row>
+                        <Col sm="6">{allPosts && allPosts.length > 0 ? <h4>{allPosts.length} Posts Found</h4> : <h4>No Post Found</h4>}</Col>
+                        <Col sm="6" className="right-align">
+                            <Dropdown>
+                                <Dropdown.Toggle variant="primary" id="dropdown-basic" size="sm" drop="left">
+                                    <FaPlus /> POST
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    {DropdownItemRender('info', 'event', 'Event')}
+                                    {DropdownItemRender('info', 'project', 'Project')}
+                                    {DropdownItemRender('info', 'general', 'General Post')}
+                                    {DropdownItemRender('info', 'volunteering', 'Volunteering')}
+                                    {DropdownItemRender('info', 'in-kind', 'In-Kind')}
+                                    {DropdownItemRender('info', 'advocacy', 'Advocacy')}
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            <br />
+                            <Button variant="primary" onClick={handleShow} size="sm">
+                                Create Post
+                            </Button>
+                        </Col>
+                    </Row>
+                    <br />
+                    {allPosts && allPosts.length > 0 && (
                         <Table striped bordered hover>
                             {/* <thead>
                                 <tr>
@@ -132,33 +131,34 @@ const PostList = (props) => {
                                 })}
                             </tbody>
                         </Table>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    } else
-        return (
-            <Container>
-                <Row>
-                    <Col className="parent-page">
-                        <Row>
-                            <Col sm="6">
-                                <h4>No Posts Found</h4>
-                            </Col>
-                            <Col sm="6" className="right-align">
-                                <Button
-                                    className="primary"
-                                    onClick={() => {
-                                        props.handleGoToPostCreate();
-                                    }}
-                                >
-                                    Create Post
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-        );
+                    )}
+                </Col>
+            </Row>
+        </Container>
+    );
+    // } else
+    //     return (
+    //         <Container>
+    //             <Row>
+    //                 <Col className="parent-page">
+    //                     <Row>
+    //                         <Col sm="6">
+    //                             <h4>No Posts Found</h4>
+    //                         </Col>
+    //                         <Col sm="6" className="right-align">
+    //                             <Button
+    //                                 className="primary"
+    //                                 onClick={() => {
+    //                                     props.handleGoToPostCreate();
+    //                                 }}
+    //                             >
+    //                                 Create Post
+    //                             </Button>
+    //                         </Col>
+    //                     </Row>
+    //                 </Col>
+    //             </Row>
+    //         </Container>
+    //     );
 };
 export default PostList;
